ec25329f7f23aeeceae22542129c0aa7806930de
renhubgit <renik2001@gmail.com>
2022-02-23 11:09:09 +0100

    added weather API functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-console.log("no")
+import weather from "./modules/weather"
+
+let searchInput = document.querySelector("#search-input")
+let searchBar = document.querySelector("#search-bar")
+searchBar.addEventListener("submit", (e) => {
+    e.preventDefault()
+    getData()
+})
+
+async function getData() {
+    let data = await weather.getData(searchInput.value)
+    console.log(data)
+}
